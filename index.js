a209a91144fd1eeade2efa447e209bfc1de2175d
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-07 21:52:37 -0700

    sum all num in array

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
+const sumItems = (array) => {
   // Sum all the numbers in the array
-}
+  let total = 0;
+  for (let num of array) {
+    if (Array.isArray(num)) {
+      total += sumItems(num);
+    } else {
+      total += num;
+    }
+  }
+  return total;
+};
 
 module.exports = sumItems;
